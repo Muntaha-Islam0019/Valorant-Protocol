6fcf1a31ec4dfba1a2aa0234e8c249c3341f0b18
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-05-16 19:59:30 +0600

    Added ability to change hero name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +11,11 @@ import { HeroesComponent } from './heroes/heroes.component';
     AppComponent,
     HeroesComponent
   ],
+  // Contains a list of external modules that the application needs
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
