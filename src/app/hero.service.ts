e6226ad52df46b555b22dc4344ff7f34d9c1974a
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-05-17 17:58:55 +0600

    Added hero service to serve the hero data

---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+// Components shouldn't fetch or save data directly and they certainly shouldn't knowingly present fake data
+// They should focus on presenting data and delegate data access to a service
+// Services are a great way to share information among classes that don't know each other
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+// This marks the class as one that participates in the dependency injection system
+// The @Injectable() decorator accepts a metadata object for the service
+// When you provide the service at the root level, Angular creates a single, shared instance of HeroService and injects into any class that asks for it
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor() {}
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 // Calling the members as heroes, as there are agents and radiants both, and obviously, they are heroes
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 // @Component is a decorator function that specifies the Angular metadata for the component
 // The CSS element selector, 'app-heroes', matches the name of the HTML element that identifies this component within a parent component's template
@@ -16,17 +16,26 @@ export class HeroesComponent implements OnInit {
   //   id: 1,
   //   name: 'Brimstone',
   // };
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() {}
+  // The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site
+  // Reserve the constructor for minimal initialization such as wiring constructor parameters to properties
+  // The constructor shouldn't do anything
+  constructor(private heroService: HeroService) {}
 
   // The ngOnInit() is a lifecycle hook
   // Angular calls ngOnInit() shortly after creating a component
   // It's a good place to put initialization logic
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
